71e0c7009fea4c3fb646b151753f4fe49a75730c
macmurp <macmurp@gmail.com>
2020-05-06 11:54:50 -0700

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace razor_input.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Greeting { get; set; }
+        public void OnGet()
+        {
+            Greeting = "Hello from the backend";
+        }
+        public void OnPost()
+        {
+
+        }
+    }
+}
